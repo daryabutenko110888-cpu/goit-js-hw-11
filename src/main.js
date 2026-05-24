4a7e2e3f3482019025af459fa7f4ef113394dca3
daryabutenko-cpu <daryabutenko110888@gmail.com>
2026-05-24 22:00:37 +0300

    my eleventh hw-js

---
+++ src/main.js
@@ -0,0 +1,61 @@
+import iziToast from 'izitoast';
+import 'izitoast/dist/css/iziToast.min.css';
+
+import {
+  createGallery,
+  clearGallery,
+  showLoader,
+  hideLoader,
+} from './js/render-functions';
+
+import { getImagesByQuery } from './js/pixabay-api';
+
+const form = document.querySelector('.form');
+
+form.addEventListener('submit', onSearch);
+
+function onSearch(event) {
+  event.preventDefault();
+
+  const query = event.currentTarget.elements['search-text'].value.trim();
+
+  if (!query) {
+    iziToast.error({
+      message: 'Please enter a search query!',
+      position: 'topRight',
+    });
+
+    return;
+  }
+
+  clearGallery();
+  showLoader();
+
+  getImagesByQuery(query)
+    .then(data => {
+      if (data.hits.length === 0) {
+        iziToast.error({
+          message:
+            'Sorry, there are no images matching your search query. Please try again!',
+          position: 'topRight',
+        });
+
+        return;
+      }
+
+      createGallery(data.hits);
+    })
+    .catch(error => {
+      iziToast.error({
+        message: 'Something went wrong!',
+        position: 'topRight',
+      });
+
+      console.log(error);
+    })
+    .finally(() => {
+      hideLoader();
+
+      form.reset();
+    });
+}
